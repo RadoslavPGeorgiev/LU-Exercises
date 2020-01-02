e33a1b4d77e9ee7faa59ee57cf7b773f502207f9
RadoslavPGeorgiev <50392748+RadoslavPGeorgiev@users.noreply.github.com>
2020-01-02 04:05:46 +0200

    Delete BankProgram.java

--- src/BankProgram.java
+++
@@ -1,9 +0,0 @@
-public class BankProgram {
-    public static void main(String[] args){
-        Bank bank = new Bank();
-        BankAccount bankAccount = new BankAccount(12345);
-        BankClient bankClient = new BankClient();
-
-        bankClient.run();
-    }
-}
